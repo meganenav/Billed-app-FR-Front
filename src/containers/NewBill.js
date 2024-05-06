649b9ef041d84833d7775a3700129d0dccf06ba7
Mégane Navarro <mnavarro@live.fr>
2024-05-06 11:19:15 +0200

    Ajout de la vérification sur l'extension du fichier d'une nouvelle note de frais

--- src/containers/NewBill.js
+++ src/containers/NewBill.js
@@ -20,25 +20,30 @@ export default class NewBill {
     const file = this.document.querySelector(`input[data-testid="file"]`).files[0]
     const filePath = e.target.value.split(/\\/g)
     const fileName = filePath[filePath.length-1]
-    const formData = new FormData()
-    const email = JSON.parse(localStorage.getItem("user")).email
-    formData.append('file', file)
-    formData.append('email', email)
+    const filePathSplit = fileName.split(".")
+    const fileExtension = filePathSplit[filePathSplit.length-1]
+    const arrayExtensions = ["jpg", "jpeg", "png"]
+    if (file && arrayExtensions.includes(fileExtension)) {
+      const formData = new FormData()
+      const email = JSON.parse(localStorage.getItem("user")).email
+      formData.append('file', file)
+      formData.append('email', email)
 
-    this.store
-      .bills()
-      .create({
-        data: formData,
-        headers: {
-          noContentType: true
-        }
-      })
-      .then(({fileUrl, key}) => {
-        console.log(fileUrl)
-        this.billId = key
-        this.fileUrl = fileUrl
-        this.fileName = fileName
-      }).catch(error => console.error(error))
+      this.store
+        .bills()
+        .create({
+          data: formData,
+          headers: {
+            noContentType: true
+          }
+        })
+        .then(({fileUrl, key}) => {
+          console.log(fileUrl)
+          this.billId = key
+          this.fileUrl = fileUrl
+          this.fileName = fileName
+        }).catch(error => console.error(error))
+    }
   }
   handleSubmit = e => {
     e.preventDefault()
